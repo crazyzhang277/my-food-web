55d76a91c2a97d0058d19832e7948c6c3e2d89b6
crazyzhang277 <js.2773612084@gmail.com>
2026-08-12 18:10:15 +0800

    feat: initialize Vite project with high-end design system tokens

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <div style={{ color: '#fff', padding: '20px' }}>App Initializing...</div>
+  </React.StrictMode>
+);
